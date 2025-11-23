5ebc692e0a36ec92c78dc06b4bec5ceaa55e1001
chunkychase1 <chaselin2024@gmail.com>
2025-11-23 04:54:57 -0800

    added function for adding score

--- src/scoreboard.ts
+++ src/scoreboard.ts
@@ -1,4 +1,13 @@
-export function setupScore (element: HTMLDivElement){
-    let score = 0
+let score = 0
+export function setupScore (element: HTMLDivElement): void{
     element.innerHTML = score.toString()
+}
+
+//adds score to scoreboard
+export function addScore (addAmount: number): void{
+    try {
+        score += addAmount
+    } catch (error) {
+        console.log(`score add error : ${error}`)
+    }
 }
